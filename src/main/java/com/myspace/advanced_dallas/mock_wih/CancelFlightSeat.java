e0b55a462a0ce8d4612e371a454879c5a7103d6f
Henry Hellbusch <hhellbusch@gmail.com>
2018-06-28 10:47:31 -0500

    mock hanlders for cancel actions

---
+++ src/main/java/com/myspace/advanced_dallas/mock_wih/CancelFlightSeat.java
@@ -0,0 +1,22 @@
+package com.myspace.advanced_dallas.mock_wih;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class CancelFlightSeat implements WorkItemHandler {
+
+  public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+    Map<String, Object> results = new HashMap<String, Object>();
+    manager.completeWorkItem(workItem.getId(), results);
+  }
+
+  public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
