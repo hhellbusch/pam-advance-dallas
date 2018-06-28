f881dfdd17758b5a8da680eb7df901e680d0a5e4
Henry Hellbusch <hhellbusch@gmail.com>
2018-06-28 10:47:58 -0500

    remove comments from mock handlers

--- src/main/java/com/myspace/advanced_dallas/mock_wih/FlightBookingWih.java
+++ src/main/java/com/myspace/advanced_dallas/mock_wih/FlightBookingWih.java
@@ -10,23 +10,7 @@ import org.kie.api.runtime.process.WorkItemManager;
 public class FlightBookingWih implements WorkItemHandler {
 
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
-		// getting params from the process
-		//	MyObject obj = (MyObject) workItem.getProcess("parameter_name");
-
-		// TODO fetch endpoint of service
-		//TODO from proc info retrive room info so it can be eval'd by service
-
-		// build response obj
 		Map<String, Object> results = new HashMap<String, Object>();
-		// results.put("output_mapping_f1", "value 1");
-		// results.put("output_mapping_f2", 2);
-
-		// TODO set value based on response from webservice
-		// results.put("vacancy", true);
-
-		// with out manager.completeWorkItem - the transaction will be put 
-		// into a wait state (saves to db)
-		// also required for passing forward results
 		manager.completeWorkItem(workItem.getId(), results);
 	}
 
